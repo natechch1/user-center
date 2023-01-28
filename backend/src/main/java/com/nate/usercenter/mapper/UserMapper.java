bdf60863a1cdaf1ab9496cf401b7faf649f23f9e
Hongce Chen <hongcechen@gmail.com>
2023-01-28 15:26:44 -0500

    Add project from local into github.

---
+++ backend/src/main/java/com/nate/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.nate.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nate.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
